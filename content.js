a4436c312ec2903114399d4f2187be3869c0ca12
Raleigh Littles III <raleighlittles@gmail.com>
2021-08-24 23:53:14 -0700

    First commit
    Stripped out stuff from last repo that wasn't needed; almost finished with download

--- content.js
+++ content.js
@@ -2,48 +2,41 @@
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
       if( request.message === "clicked_browser_action" ) {
+          // Looks something like: https://<website>.jpg?w=<size>
+          // We need to strip off everything after the image extension.
+          const imgUrlUntruncated = document.querySelector("meta[property=og\\:image]")["content"];
 
-          // The GraphQL data location is different depending on whether the user is logged in or not. (Why?)
-          const isUserLoggedIn = (document.querySelector("html").classList[1] !== "not-logged-in");
-          var graphQlMediaObj;
+          const postAuthor = document.querySelector('meta[property=og\\:title]')["content"];
 
-          // The two regular expressions that follow are shamelessly taken from: https://github.com/instaloader/instaloader
-          if (isUserLoggedIn) {
-              postInfoObj = JSON.parse(document.documentElement.outerHTML.match(/<script type="text\/javascript">window\.__additionalDataLoaded\(.*?({.*"graphql":.*})\);<\/script>/)[1]);
-              graphQlMediaObj = postInfoObj.graphql.shortcode_media;
-          }
-           else {
-              postInfoObj = JSON.parse(document.documentElement.outerHTML.match(/<script type="text\/javascript">window\._sharedData = (.*)<\/script>/)[1].slice(0, -1));
-              graphQlMediaObj = postInfoObj.entry_data.PostPage[0].graphql.shortcode_media;
-          }
+          const postDateObj = document.querySelector("time");
 
-           // Easy case -- post consists of a single image or video.
-          if (graphQlMediaObj.edge_sidecar_to_children == null)
-          {
-              // Video posts have a 'display_url' element too, which is just a thumbnail.
-              downloadMediaFromPost((graphQlMediaObj.is_video === false) ? graphQlMediaObj.display_url : graphQlMediaObj.video_url,
-                  constructDownloadedFilename(graphQlMediaObj.owner.username,
-                                              graphQlMediaObj.taken_at_timestamp,
-                                    (graphQlMediaObj.is_video === true) ? "vid" : "img"));
-          }
+          // Date is of the form "<Month Name> <Day>, <Year>"
+          // Time is of the form: "AB:CD <A|P>M"
 
-          else // Post has either multiple videos, multiple images, or some combination of both.
-              for (var i = 0; i < graphQlMediaObj.edge_sidecar_to_children.edges.length; i++) {
+          const postDateRaw = postDateObj.children[0].innerText;
+          const postTimeRaw = postDateObj.children[1].innerText;
 
-                  const subpostObj = graphQlMediaObj.edge_sidecar_to_children.edges[i].node;
+          const postDateJSObj = Date.parse("".concat(postDateRaw, " ", postTimeRaw));
+          const today = new Date();
+          const timestamp = 'DA_'.concat(today.getFullYear(),
+                                    '-', today.getMonth() + 1,
+                                    '-', today.getDate(),
+                                    'T', today.getHours(),
+                                    '', today.getMinutes(),
+                                    '', today.getSeconds());
+        
+         
+        // Call chrome.runtime.sendMessage directly
+          
 
-                  downloadMediaFromPost((subpostObj.is_video === false) ? subpostObj.display_url : subpostObj.video_url,
-                      constructDownloadedFilename(graphQlMediaObj.owner.username,
-                                                  graphQlMediaObj.taken_at_timestamp,
-                                        (subpostObj.is_video === true) ? "vid" : "img"));
-          }
+
       }
 
       function downloadMediaFromPost(mediaUrl, filenameToSaveAs) {
           chrome.runtime.sendMessage({mediaUrl: mediaUrl, filename: filenameToSaveAs});
       }
 
-      function constructDownloadedFilename(author, dateUploadedUnixTs, mediaFmt) {
+      function constructDownloadedFilename(author, dateUploadedUnixTs) {
           const today = new Date();
           const timestamp = 'DA_'.concat(today.getFullYear(),
                                   '-', today.getMonth(),
@@ -52,11 +45,11 @@ chrome.runtime.onMessage.addListener(
                                   '', today.getMinutes(),
                                   '', today.getSeconds());
 
-          // Unix epochs/timestamps are in units of seconds, whereas javascript date objects use milliseconds.
+          // Unix epochs/timestamps are in units of seconds, whereas Javascript date objects use milliseconds.
           const uploadDateObj = new Date(dateUploadedUnixTs * 1000);
 
           const uploadDate = 'DC_'.concat(uploadDateObj.getFullYear(),
-              '-', uploadDateObj.getMonth(),
+              '-', uploadDateObj.getMonth() + 1, // Javascript months are 1-indexed, while days within months are 0-indexed
               '-', uploadDateObj.getDate(),
               'T', uploadDateObj.getHours(),
               '', uploadDateObj.getMinutes(),
