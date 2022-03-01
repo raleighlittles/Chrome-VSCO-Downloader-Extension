632978b3246523fbcd68c2fadd5024c48ff4dbf4
Raleigh Littles III <raleighlittles@gmail.com>
2022-03-01 12:55:10 -0800

    switch to manifest v3

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 // background.js
 
 // Called when the user clicks on the browser action.
-chrome.browserAction.onClicked.addListener(function(tab) {
+chrome.action.onClicked.addListener(function(tab) {
     // Send a message to the active tab
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
       var activeTab = tabs[0];
