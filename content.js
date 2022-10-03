b7c9b67d6f760923b81247cfe73f485aca9cae62
Raleigh Littles <raleighlittles@gmail.com>
2022-10-03 08:59:32 -0700

    added simple workaround around issue with username
    Currently, the 'postAuthor' field inadvertedly includes part of the caption if there is on one. As a simple workaround, this checks if the postAuthor field has a space in it ( usernames aren't allowed to have spaces in them ), and only if it doesnt will the username will be included in the resulting filename for the image saved.

--- content.js
+++ content.js
@@ -1,58 +1,47 @@
-// content.js
+// File: content.js
 chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        if (request.message === "clicked_browser_action") {
+            // Looks something like: https://<website>.jpg?w=<size>
+            // We need to strip off everything after the image extension.
+            const imgUrlUntruncated = document.querySelector("meta[property=og\\:image]")["content"];
+            const imgUrl = imgUrlUntruncated.split("?")[0];
+            console.log("Downloading from URL: ", imgUrl);
 
-  function(request, sender, sendResponse) {
-      if (request.message === "clicked_browser_action") {
+            const postAuthor = document.querySelector('meta[property=og\\:title]')["content"];
 
-          // Start by gathering metadata for the post
-          const postAuthor = document.querySelector('meta[property=og\\:title]')["content"];
-          const postDateObj = document.querySelector("time");
-          // Date is of the form "<Month Name> <Day>, <Year>"
-          const postDateRaw = postDateObj.children[0].innerText;
+            const postDateObj = document.querySelector("time");
 
-          // TODO: Include the time the post was uploaded
-          // Take the raw string date on the page, and convert it to a unix epoch, then convert that unix epoch to a datetime object
-          const postDateJSObj = new Date(Date.parse(postDateRaw));
+            // Date is of the form "<Month Name> <Day>, <Year>"
+            const postDateRaw = postDateObj.children[0].innerText;
+
+            // TODO: Include the time the post was uploaded
+            // Take the raw string date on the page, and convert it to a unix epoch, then convert that unix epoch to a datetime object
+            const postDateJSObj = new Date(Date.parse(postDateRaw));
+
+            // Store the date the file was downloaded
+            const today = new Date();
+            const timestamp = 'DA_'.concat(today.getFullYear(),
+                '-', today.getMonth() + 1,
+                '-', today.getDate(),
+                'T', today.getHours(),
+                '', today.getMinutes(),
+                '', today.getSeconds());
+
+            const safeFilenameRegex = "/[\/:*|?\"<>|]/g";
+
+            // Call chrome.runtime.sendMessage directly
+            const newFilename = ("".concat("vsco__", postAuthor.indexOf(' ') > 0 ? "" : postAuthor, "__", timestamp, "__", 'DC'.concat(postDateJSObj.getFullYear(),
+                postDateJSObj.getMonth() + 1,
+                postDateJSObj.getDay()), ".jpg"));
 
 
-          // Check if the post is a video by searching for a video URL. if no video element is found, you can assume its a photo
-          // This can't be done the other way around, since video posts do include image URLs (for the thumbnail)
-          const videoUrl = document.querySelector("meta[property=og\\:video\\:secure_url")["content"];
+            console.log("Downloading file to location: ", newFilename);
 
-          // Image URLs something like: https://<website>.jpg?w=<size>
-          // We need to strip off everything after the image extension.
-          const imgUrlUntruncated = document.querySelector("meta[property=og\\:image]")["content"];
-          const imgUrl = imgUrlUntruncated.split("?")[0];
-
-          var postType;
-
-          if (videoUrl != undefined) {
-              postType = "v"; // video
-              console.log("Downloading video from URL: ", videoUrl);
-          } else {
-              postType = "i"; // image
-              console.log("Downloading image from URL: ", imgUrl);
-          }
-
-          chrome.runtime.sendMessage({
-              mediaUrl: postType == 'v' ? videoUrl : imgUrl,
-              filename: constructDownloadedFilename(postAuthor, postDateJSObj, postType)
-          });
-      }
-
-      function constructDownloadedFilename(author, dateUploadedObj, mediaFmt) {
-          // Store the date the file was downloaded
-          const today = new Date();
-          const timestamp = 'DA_'.concat(today.getFullYear(),
-              '-', today.getMonth() + 1,
-              '-', today.getDate(),
-              'T', today.getHours(),
-              '', today.getMinutes(),
-              '', today.getSeconds());
-
-          return "vsco__".concat(author, "_", timestamp, "__", 'DC'.concat(dateUploadedObj.getFullYear(), "-", dateUploadedObj.getMonth() + 1, "-", dateUploadedObj.getDate(), "T", today.getHours(), today.getMinutes(), today.getSeconds(), mediaFmt == "v" ? ".mp4" : ".jpg"));
-
-        } // end constructDownloadedFilename
-
-      } // end listener function
+            chrome.runtime.sendMessage({
+                mediaUrl: imgUrl,
+                filename: newFilename
+            });
+        }
+    } // end function
 );
